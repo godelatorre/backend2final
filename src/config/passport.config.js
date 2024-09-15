74450d701ec5eecf90c8711574ce5f2dad9286d5
gdelatorre <g18delatorre@gmail.com>
2024-09-14 23:28:47 -0300

    ultimos detalles

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -20,15 +20,9 @@ const CustomStrategy = passportCustom.Strategy;
 
 export const initializePassport = () => {
   passport.use(
-    "register", // Nombre de la estrategia
+    "register", //Estrategia
     new LocalStrategy({ passReqToCallback: true, usernameField: "email" }, async (req, username, password, done) => {
-      /* 
-      "register" es el nombre de la estrategia que estamos creando.
-      passReqToCallback: true, nos permite acceder a la request en la función de autenticación.
-      usernameField: "email", nos permite definir el campo que usaremos como username.
-      done es una función que debemos llamar cuando terminamos de procesar la autenticación.
-      Nota: passport recibe dos datos el username y el password, en caso de que no tengamos un campo username en nuestro formulario, podemos usar usernameField para definir el campo que usaremos como username.
-      */
+      
       try {
         const { first_name, last_name, age } = req.body;
         const user = await userRepository.getByEmail(username);
@@ -69,7 +63,7 @@ export const initializePassport = () => {
     })
   );
 
-  // Estrategia de Google
+  // Google
   passport.use(
     "google",
     new GoogleStrategy(
@@ -102,7 +96,7 @@ export const initializePassport = () => {
     )
   );
 
-  // Estrategia de JWT
+  // JWT
   passport.use(
     "jwt",
     new JWTStrategy(
@@ -140,12 +134,6 @@ export const initializePassport = () => {
   )
 
   // Serialización y deserialización de usuarios
-  /* 
-  La serialización y deserialización de usuarios es un proceso que nos permite almacenar y recuperar información del usuario en la sesión.
-  La serialización es el proceso de convertir un objeto de usuario en un identificador único.
-  La deserialización es el proceso de recuperar un objeto de usuario a partir de un identificador único.
-  Los datos del user se almacenan en la sesión y se recuperan en cada petición.
-  */
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
